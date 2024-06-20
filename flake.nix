118c822c9f400ca003334679fe59b7988c067bdf
Viktor Bruggeman <viktor.bruggeman@epitech.eu>
2024-06-20 14:05:45 +0200

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "My Neovim config using Nixvim";
+
+  inputs = {
+    nixvim.url = "github:nix-community/nixvim";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    nixvim,
+    flake-utils,
+    ...
+  } @ inputs: let
+    config = import ./config;
+  in
+    flake-utils.lib.eachDefaultSystem (system: let
+      nixvimLib = nixvim.lib.${system};
+      pkgs = import nixpkgs {inherit system;};
+      nixvim' = nixvim.legacyPackages.${system};
+      nvim = nixvim'.makeNixvimWithModule {
+        inherit pkgs;
+        module = config;
+        extraSpecialArgs = {
+          inherit self;
+        };
+      };
+    in {
+      checks = {
+        default = nixvimLib.check.mkTestDerivationFromNvim {
+          inherit nvim;
+          name = "nvim";
+        };
+      };
+
+      packages = {
+        default = nvim;
+      };
+
+      formatter = pkgs.alejandra;
+    });
+}
