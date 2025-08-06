9a561959dcc8a864f934f6a90bc1dcbde9cff52a
Viktor Bruggeman <viktor.bruggeman@epitech.eu>
2025-08-06 18:19:38 +0200

    feat: add integration for catppuccin

--- config/lua/hinivim/plugins/themes.lua
+++ config/lua/hinivim/plugins/themes.lua
@@ -11,6 +11,49 @@ return {
           dark = "mocha",
         },
         term_colors = true,
+        integrations = {
+          alpha = true,
+          treesitter = true,
+          treesitter_context = false,
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+            },
+            underlines = {
+              errors = { "undercurl" },
+              hints = { "undercurl" },
+              warnings = { "undercurl" },
+              information = { "undercurl" },
+            },
+          },
+          telescope = {
+            enabled = true,
+          },
+          cmp = false,
+          blink_cmp = true,
+          lsp_trouble = true,
+          nvimtree = true,
+          which_key = false,
+          indent_blankline = {
+            enabled = true,
+            colored_indent_levels = true,
+          },
+          navic = {
+            enabled = true,
+            custom_bg = "NONE",
+          },
+          gitsigns = true,
+          markdown = true,
+          render_markdown = false,
+          harpoon = true,
+          symbols_outline = true,
+          ts_rainbow = true,
+          notify = true,
+        },
       })
     end,
   }
