5cf629aea524dc4618070b932c3e7adbd457622e
Viktor Bruggeman <viktor.bruggeman@epitech.eu>
2025-08-14 09:52:59 +0200

    feat: remove undotree

--- config/lua/hinivim/plugins/init.lua
+++ config/lua/hinivim/plugins/init.lua
@@ -84,15 +84,6 @@ return {
   },
 
   {
-    "undotree",
-    event = "BufReadPost",
-    after = function()
-      require("undotree").setup()
-      vim.keymap.set("n", "<leader>u", require("undotree").toggle, { noremap = true, silent = true })
-    end,
-  },
-
-  {
     "direnv.nvim",
     event = "VimEnter",
     enabled = function()
